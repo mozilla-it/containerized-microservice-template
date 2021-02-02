de4468b143566aca44287f401b9ead2e318c7ef1
Bryan Sieber <bsieber@mozilla.com>
2021-02-01 16:30:14 -0800

    Pre-commit, and linting synch'd to same dep versions--in diff files--as well as new baseline, modifications to docker build, lint script updates...

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,7 +6,7 @@ CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 BASE_DIR="$(dirname "$CURRENT_DIR")"
 
 bandit -lll --recursive "${BASE_DIR}" --exclude "${BASE_DIR}/poetry.lock,${BASE_DIR}/.venv,${BASE_DIR}/.mypy,${BASE_DIR}/build"
-detect-secrets audit .secrets.baseline
+detect-secrets-hook {./*,**/**} --baseline .secrets.baseline
 mypy --no-strict-optional --ignore-missing-imports "${BASE_DIR}"
 black --config "${BASE_DIR}/pyproject.toml" "${BASE_DIR}"
 isort --recursive --settings-path "${BASE_DIR}/pyproject.toml" "${BASE_DIR}"
